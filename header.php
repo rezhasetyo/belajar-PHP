8d7e1df26477c5d4286da2bdd76beb718de7b97b
rezhasetyo <rezhasetyo87@gmail.com>
2022-04-29 22:34:27 +0700

    refac code include header/footer

--- header.php
+++ header.php
@@ -1,60 +1,39 @@
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Belajar Bootstrap 4 : DTS 2020</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <a class="navbar-brand" href=""> Daftar Hutang ter-Update </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    <!-- Menyisipkan Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item <?php if ($hal=='') echo "active";?>">
+        <a class="nav-link" href="index.php">Depan <span class="sr-only">(current)</span></a>
+      </li>
 
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="">Daftar Hutang ter-Update     
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
- 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item
-            <?php
-              if ($hal=='')
-                echo "active";
-            ?>
-          ">
-            <a class="nav-link" href="index.php">Depan <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item
-          <?php
-              if ($hal=='form')
-                echo "active";
-            ?>
-          ">
-            <a class="nav-link" href="?hal=form">Form</a>
-          </li>
-          <li class="nav-item
-          <?php
-              if ($hal=='data')
-                echo "active";
-            ?>
-          ">
-            <a class="nav-link" href="?hal=data">Data</a>
-          </li>
-          <!--
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Programming</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Bootstrap</a>
-              <a class="dropdown-item" href="#">CSS</a>
-              <a class="dropdown-item" href="#">HTML 5</a>
-            </div>
-          </li>
-          -->
-        </ul>
-        
-     <!--   <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      -->  
-      </div>
-    </nav>
+      <li class="nav-item <?php if ($hal=='form') echo "active";?>">
+        <a class="nav-link" href="?hal=form">Form</a>
+      </li>
+      
+      <li class="nav-item <?php if ($hal=='data') echo "active";?>">
+        <a class="nav-link" href="?hal=data">Data</a>
+      </li>
+      
+      <!--
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Programming</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
+          <a class="dropdown-item" href="#">Bootstrap</a>
+          <a class="dropdown-item" href="#">CSS</a>
+          <a class="dropdown-item" href="#">HTML 5</a>
+        </div>
+      </li>
+      -->
+    </ul>
+    
+ <!--   <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  -->  
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php
   $hal = $_GET['hal'];
-  include "koneksi.php";
-?>
+  include "koneksi.php";  ?>
+
 <!DOCTYPE html>
 <html>
-
 <head>
-    <?php
-      include "header.php";
-    ?>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Belajar Bootstrap 4 : DTS 2020</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css" />      <!-- Menyisipkan Link Style CSS Bootstrap -->
+  <?php include "header.php"; ?>                              <!-- MENYISIPKAN HEADER -->
 </head>
 
 <body>
-    <?php
-      if(isset($hal))
-      {
-        //jika $hal ada isinya
-        include $hal.".php";
-      }else{
-        include "depan.php";
-      }
-    ?>
+  <?php
+    if(isset($hal)) {
+      //jika $hal ada isinya
+      include $hal.".php";
+    }else {
+      include "depan.php";  } ?>
 </body>
 
-    <?php
-      include "footer.php";
-    ?>
-
+<!-- Menyisipkan Link Jquery Bootstrap -->
+  <script src="js/jquery-3.4.1.slim.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+ <!-- MENYISIPKAN FOOTER -->
+  <?php include "footer.php"; ?>     
 </html>
